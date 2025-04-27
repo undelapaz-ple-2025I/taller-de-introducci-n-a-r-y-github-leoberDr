9e43f71df6937fa73729eac2d5fb6cb044106e1f
Leober Duran <lduranr@unal.edu.co>
2025-04-27 12:17:41 -0500

    Update ejercicio08.R

--- ejercicio08.R
+++ ejercicio08.R
@@ -7,8 +7,8 @@ solucion <- function() {
   b <- 8
   c <- 5
   d <- 3
-  x <- NULL # ingresa valor aquí
-  y <- NULL # ingresa valor aquí
+  x <- 3 # ingresa valor aquí
+  y <- 4 # ingresa valor aquí
   s <- a*x-b
   t <- 2*s+c-d
   return(c(s,t,x,y))
