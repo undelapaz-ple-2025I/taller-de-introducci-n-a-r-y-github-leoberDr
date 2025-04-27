4062d3d9dad207f5347f917819d0e32f15967633
Leober Duran <lduranr@unal.edu.co>
2025-04-27 11:58:14 -0500

    Update ejercicio05.R

--- ejercicio05.R
+++ ejercicio05.R
@@ -2,9 +2,10 @@
 
 solucion <- function() {
   # Ordena (sin realizar nuevas asignaciones) de manera que se devuelva un valor de 's' diferente a NULL
+  s <- NULL
   a <- 10
   b <- 20
   s <- a+b
-  s <- NULL
+  
   return(s) 
 }
